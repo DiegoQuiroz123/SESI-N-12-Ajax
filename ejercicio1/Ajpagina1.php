323299e29dbfd21d0e6b06f1d38d25206eae356f
Rony Quiroz <diegoquirozhuerta123@outlook.com>
2021-11-30 09:40:39 -0500

    Se subio experiencia 1 y el ejercicio 1

---
+++ ejercicio1/Ajpagina1.php
@@ -0,0 +1,17 @@
+<?PHP
+if ($_REQUEST['cod']==1)
+ echo "<strong>Mouse logitech420:</strong> Mouse gamer alta gama, 4000 dpi y flexible
+Precio: S/.220.";
+if ($_REQUEST['cod']==2)
+ echo "<strong>Pantalla LG '42 pulgadas:</strong> Pantalla LG con excelente resolucion, con Netflix y Youtube
+Precio: S/.2099.";
+if ($_REQUEST['cod']==3)
+ echo "<strong>Audifonos Sony:</strong> Audifonos inalambricos de Sony, autonomia de bateria hasta 9 horas seguidas
+Precio: S/.120.";
+if ($_REQUEST['cod']==4)
+ echo "<strong>Teclado Red Dragon X40:</strong> Teclado suave y para largas horas de trabajo o juego, cuenta con luces led en las teclas
+Precio: S/.150.";
+if ($_REQUEST['cod']==5)
+ echo "<strong>Cargador Apple 13:</strong> Cargador para el ultimo celular sacado por la marca de la manza, cuenta con 200W para una carga rapida y amigable
+Precio: S/.90.";
+
